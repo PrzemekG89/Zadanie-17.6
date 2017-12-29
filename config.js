717b2a85ccb56832c1f51c92c438d1146180be86
Przemek Gałdys <galdys.przemek@gmail.com>
2017-12-29 22:29:51 +0100

    Zadanie 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '245428173474-8s145hn95hotanojsnkuv2obqpsk3avn.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'aP--UFqG6rQ1NOaXiU1QOZ9u',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,24 +1,55 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/static', express.static('assets'));
 
+//app routes
 app.get('/', function (req, res) {
-    res.render('index', {
-        user: 
-        {username: "Przemek", password: "1234"}
-    });
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', function (req, res) {
-    res.render('aplication');
+app.get('/logged', function (req, res) {
+    res.render('logged', { user: googleProfile });
 });
+//Passport routes
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/logged',
+        failureRedirect: '/'
+    }));
 
-app.listen(3003);
-
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+app.listen(3000);
